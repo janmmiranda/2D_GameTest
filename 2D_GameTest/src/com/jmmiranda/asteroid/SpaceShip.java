bee1f4cd773a86b8a839151eb2f454becc39afb0
Jan Matthew Miranda <janmatthewmiranda@Jans-MacBook-Pro.local>
2018-07-24 21:29:32 +0530

    Create space ship alien shooter game

---
+++ 2D_GameTest/src/com/jmmiranda/asteroid/SpaceShip.java
@@ -0,0 +1,67 @@
+package com.jmmiranda.asteroid;
+
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+
+public class SpaceShip extends Sprite{
+
+	private int dx;
+    private int dy;
+    private List <Missile> missiles;
+
+    public SpaceShip(int x, int y) {
+    	super(x,y);
+    	
+        initSpaceShipe();
+    }
+
+    private void initSpaceShipe() {
+    	missiles = new ArrayList <Missile>();
+    	
+		loadImage("src/resources/craft.png");
+		getImageDimensions();
+	}
+
+	public void move() {     
+        x += dx;
+        y += dy;
+        
+        if (x < 1) { x = 1; }
+        if (y < 1) { y = 1; }
+    }
+	
+	public List<Missile> getMissiles() {
+		return missiles;
+	}
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+        
+        if (key == KeyEvent.VK_SPACE) { 
+        	fire(); 
+        	System.out.println("fires");
+        }
+        if (key == KeyEvent.VK_LEFT) { dx = -1; }
+        if (key == KeyEvent.VK_RIGHT) { dx = 1; }
+        if (key == KeyEvent.VK_UP) { dy = -1; } 
+        if (key == KeyEvent.VK_DOWN) { dy = 1; }
+    }
+
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }
+	
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+		
+		if (key == KeyEvent.VK_LEFT) { dx = 0; }
+		if (key == KeyEvent.VK_RIGHT) { dx = 0; }
+		if (key == KeyEvent.VK_UP) { dy = 0; }
+		if (key == KeyEvent.VK_DOWN) { dy = 0; }
+	}
+}
